3ba52492b3e33b80e4b6a5faa69a91d8234fd4dc
Mike Lovely <mike-lovely@hotmail.co.uk>
2016-09-23 23:27:24 +0100

    implement channel policy and request form for editing/updating channels

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('channel', 'ChannelSettingsController', ['only' => ['edit', 'update']]);
+});
