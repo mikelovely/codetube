34176858078b26baaff37261c8f18c5d6eba7d4e
Mike Lovely <mike-lovely@hotmail.co.uk>
2016-09-25 18:27:51 +0100

    add was credentials and api creds to aws

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,10 +56,18 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => 'your-key',
-            'secret' => 'your-secret',
-            'region' => 'your-region',
+            'key' => env('S3_KEY'),
+            'secret' => env('S3_SECRET'),
+            'region' => 'us-west-2',
             'bucket' => 'your-bucket',
+        ],
+
+        's3images' => [
+            'driver' => 's3',
+            'key' => env('S3_KEY'),
+            'secret' => env('S3_SECRET'),
+            'region' => 'us-west-2',
+            'bucket' => 'mikelovely-codetube',
         ],
 
     ],
